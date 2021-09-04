d26eea0e1bc78fc5ef3d17b4dbc32ee051999455
zayedibnibrahim <zayed.ibn@gmail.com>
2021-09-04 18:24:31 +0600

    Created header footer

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Layout.module.css'
+import Footer from './Footer'
+import Header from './Header'
+export default function Layout({ description, title, keywords, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      <Header></Header>
+      <div className={styles.container}>{children}</div>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'DJ Events | Find the hottest parties',
+  description: 'Find the latest DJ and other musical events',
+  keywords: 'music, dj, edm, events',
+}
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/dist/client/router'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
+  const router = useRouter()
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Layout>
+      <h1>Hello, This is about</h1>
+      <Link href='/'>Home</Link>
+    </Layout>
   )
 }
